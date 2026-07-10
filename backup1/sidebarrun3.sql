09f3144650750abc7f7af4b1c0a0027f623f11e7
Fredelson Pascua <149922916+Fredelson@users.noreply.github.com>
2026-07-10 13:22:49 +0400

    deleted schemas and text

--- backup1/sidebarrun3.sql
+++
@@ -1,8 +0,0 @@
-USE OperationsPlatformDB;
-GO
-
-DELETE FROM dbo.MenuGroupItems;
-DELETE FROM dbo.UserMenuPreferences;
-DELETE FROM dbo.Menus;
-DELETE FROM dbo.MenuGroups;
-GO
--- updatedschemaandseed/itassets/runbeforeimported.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE dbo.ITAssetAssignments
-ADD ReturnNotes NVARCHAR(MAX) NULL,
-    ReturnConditionId INT NULL,
-    ReturnIssueTypeId INT NULL,
-    ReturnIssuePriority NVARCHAR(50) NULL;
